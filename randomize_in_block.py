5bef6aceeb115f8b59e27aecf29d7708a4f450e3
indi <indivicivet@gmail.com>
2023-11-24 00:18:40 +0000

    add "pixel shuffling in block" code

---
+++ randomize_in_block.py
@@ -0,0 +1,28 @@
+import argparse
+
+from PIL import Image
+import numpy as np
+
+
+def shuffle(arr):
+    pixels = arr.reshape((-1, 3))
+    return np.random.default_rng().permuted(pixels, axis=0).reshape(arr.shape)
+
+
+def randomize_in_block(img, n_blocks=48):
+    n_blocks_y = int(img.height / img.width * n_blocks)
+    arr = np.array(img.convert("RGB"))
+    return Image.fromarray(np.vstack([
+        np.hstack([
+            shuffle(block)
+            for block in np.array_split(v_blocks, n_blocks, axis=1)
+        ])
+        for v_blocks in np.array_split(arr, n_blocks_y, axis=0)
+    ]))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_path")
+    args = parser.parse_args()
+    randomize_in_block(Image.open(args.input_path)).show()
