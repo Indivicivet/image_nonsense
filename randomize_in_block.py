7ad47ff62cb500585546d200e6b0bd978db89b40
indi <indivicivet@gmail.com>
2023-12-31 17:57:50 +0000

    shuffle colours together (by default)

--- randomize_in_block.py
+++ randomize_in_block.py
@@ -6,19 +6,27 @@ import numpy as np
 from tqdm import tqdm
 
 
-def shuffle(arr):
+def shuffle(arr, together):
+    """
+    `together` dictates if colours are kept together, or if rgb can split up
+    """
     pixels = arr.reshape((-1, 3))
-    return np.random.default_rng().permuted(pixels, axis=0).reshape(arr.shape)
+    shuffled = (
+        pixels[np.random.default_rng().random(pixels.shape[0]).argsort()]
+        if together
+        else np.random.default_rng().permuted(pixels, axis=0)
+    )
+    return shuffled.reshape(arr.shape)
 
 
-def randomize_in_block(img, n_blocks=48, max_res=720):
+def randomize_in_block(img, n_blocks=48, max_res=720, together=True):
     if img.width > max_res:
         img = img.resize((max_res, int(img.height / img.width * max_res)))
     n_blocks_y = int(img.height / img.width * n_blocks)
     arr = np.array(img.convert("RGB"))
     return Image.fromarray(np.vstack([
         np.hstack([
-            shuffle(block)
+            shuffle(block, together=True)
             for block in np.array_split(v_blocks, n_blocks, axis=1)
         ])
         for v_blocks in np.array_split(arr, n_blocks_y, axis=0)
